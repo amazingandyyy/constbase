c386a554c9eb6ee861c52a896a0d0f56f358d656
Amazingandyyy <amazingandyyy@gmail.com>
2016-07-03 01:20:46 -0700

    add vertical-align
    add vertical-align

--- constbase.js
+++ constbase.js
@@ -1,8 +1,20 @@
 'use strict';
 
 // let you write css in className
-$(function() {
+$(document).ready(init)
+
+function init() {
+    cssClassActivate()
+    applyingCSS()
+}
+
+function applyingCSS() {
+}
+
+
+
+function cssClassActivate() {
     for (var i = 0; i < document.querySelectorAll("div").length; i++) {
-        $(document.querySelectorAll("div")[i]).attr('style', document.querySelectorAll("div")[i].className.split(' ').join('; '))
+        $(document.querySelectorAll("div")[i]).attr('style', document.querySelectorAll("div")[i].className.split(' ').join('; ').split('_').join(' '))
     }
-})
+}
